839566d65e6594c9f6385e1691ebb0e4b80ec564
SunderreddySathwik <sathwik.sunderreddy@gmail.com>
2024-04-20 18:56:48 +0530

    initial commit

---
+++ BookApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult PdfReader()
+        {
+            ViewBag.Message = "Test";
+            return View("~/Views/Home/PdfReader.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
